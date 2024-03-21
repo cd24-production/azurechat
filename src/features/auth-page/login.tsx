7fb95df703a3fa09038776d40b2ed1908de4477f
SAILLARD Corentin <c.saillard@dordogne.fr>
2024-03-21 21:40:43 +0100

    Ajouter l'effet d'utilisation pour se connecter avec Azure AD

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -16,7 +16,15 @@ interface LoginProps {
   isDevMode: boolean;
 }
 
+import { useEffect } from "react";
+
 export const LogIn: FC<LoginProps> = (props) => {
+
+  useEffect(() => {
+    signIn("azure-ad");
+  }, []);
+
+
   return (
     <Card className="flex gap-2 flex-col min-w-[300px]">
       <CardHeader className="gap-2">
@@ -30,15 +38,7 @@ export const LogIn: FC<LoginProps> = (props) => {
           Login in with your GitHub or Microsoft 365 account
         </CardDescription>
       </CardHeader>
-      <CardContent className="grid gap-4">
-        <Button onClick={() => signIn("github")}>GitHub</Button>
-        <Button onClick={() => signIn("azure-ad")}> Microsoft 365</Button>
-        {props.isDevMode ? (
-          <Button onClick={() => signIn("localdev")}>
-            Basic Auth (DEV ONLY)
-          </Button>
-        ) : null}
-      </CardContent>
+
     </Card>
   );
 };
